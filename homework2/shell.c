8ca78462e9dae402594151a49a3fbe72f478c31f
bjf73558 <bjf73558@huskies.bloomu.edu>
2017-03-05 16:56:57 -0500

    Initial work on homework2

---
+++ homework2/shell.c
@@ -0,0 +1,93 @@
+/** 
+ * Brian Fekete
+ * 3/10/17
+ */
+#include <sys/types.h>
+#include <stdio.h>	
+#include <unistd.h> // chdir
+#include <string.h> // strtok
+
+#define MAX_LINE 80 /* The maximum length command */
+#define BUFFER_SIZE 25
+#define READ_END 0
+#define WRITE_END 1
+
+int main(void) {
+  printf("Welcome to Brian Fekete's Shell\n");
+  
+  char *line[MAX_LINE];
+  char *command[MAX_LINE/2];
+  char *args[MAX_LINE/2 + 1]; /* command line arguments */
+  int should_run = 1; /* Flag to determine when to exit program */
+  
+  while(should_run){
+    printf("bfek>");
+    fflush(stdout);
+    
+    fgets(line, sizeof(line), stdin);
+    
+    // Parse line and split into command and args
+    
+    
+    /**
+     * After reading user input, the steps are:
+     * (1) Fork a child process using fork()
+     * (2) The child process will invoke execvp()
+     * (3) If command included &, parent will invoke wait()
+     */
+     if(strncmp(line, "exit", 4) == 0){
+       should_run = 0;
+     } 
+     //else if(){
+     //
+     //} 
+     else {
+     
+      char write_msg[BUFFER_SIZE] = "Greetings";
+      char read_msg[BUFFER_SIZE];
+      
+      int fd[2];
+      /* Create the pipe */
+      if (pipe(fd) == -1) {
+        fprintf(stderr, "Pipe Failed");
+        return 1;
+      }
+      
+      pid_t pid;
+      pid = fork;
+      
+      if (pid < 0) { /* error occurred */
+        fprintf(stderr, "Fork failed");
+        return 1;
+      } else if (pid > 0){ /* Parent Process */
+        /* Parent will wait for the child to complete */
+        wait(NULL);
+        
+        /* close the unused end of the pipe */
+        close(fd[READ_END]);
+        
+        /* Write to the pipe */
+        write(fd[WRITE_END], write_msg, strlen(write_msg) + 1);
+        
+        /* Close the write end of the pipe */
+        close(fd[WRITE_END]);
+        
+      } else if (pid == 0) { /* Child Process */
+        /* close the unused end of the pipe */
+        close(fd[WRITE_END]);
+        
+        /* Read from the pipe */
+        read(fd[READ_END], read_msg, BUFFER_SIZE);
+        printf("read %s\n", read_msg);
+        
+        /* Close the read end of the pipe */
+        close(fd[READ_END]);  
+      }
+       
+     }
+     
+  }
+
+  return 0;
+}
+
